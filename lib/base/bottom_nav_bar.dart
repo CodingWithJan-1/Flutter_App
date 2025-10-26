8100d3c4b4d836675b48306d8127c7a549f75d64
Jan 3rd bitoon <jbitoon03@gmail.com>
2025-10-26 17:21:50 +0800

    convert the component to stateful

--- lib/base/bottom_nav_bar.dart
+++ lib/base/bottom_nav_bar.dart
@@ -1,48 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:fluentui_icons/fluentui_icons.dart';
 
-
-class BottomNavBar extends StatelessWidget {
+class BottomNavBar extends StatefulWidget {
   const BottomNavBar({super.key});
 
-  /*
+  @override
+  State<BottomNavBar> createState() => _BottomNavBar();
+}
 
-    Created a new Component BottomNavBar and added two
-    Icon Using BottomNavigationBar Package
-
-  */
+class _BottomNavBar extends State<BottomNavBar> {
+  final appScreens = [
+    const Text("Home"),
+    const Text("Search"),
+    const Text("Tickets"),
+    const Text("Profile"),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Center( 
-          child: Text('My Tickets')),
-      ),
-      body: Center(
-        child: Text('Welcome to the Ticket App!'),
-      ),
+      appBar: AppBar(title: const Center(child: Text('My Tickets'))),
+      body: Center(child: Text('Welcome to the Ticket App!')),
       bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor:  Colors.blueGrey,
+        selectedItemColor: Colors.blueGrey,
         unselectedItemColor: const Color(0xFF526400),
         items: const [
           BottomNavigationBarItem(
             icon: Icon(FluentSystemIcons.ic_fluent_home_add_regular),
             activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
-            label: "Home"),
+            label: "Home",
+          ),
           BottomNavigationBarItem(
             icon: Icon(FluentSystemIcons.ic_fluent_search_regular),
             activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
-            label: "Search"),
+            label: "Search",
+          ),
           BottomNavigationBarItem(
             icon: Icon(FluentSystemIcons.ic_fluent_airplane_regular),
             activeIcon: Icon(FluentSystemIcons.ic_fluent_airplane_filled),
-            label: "Tickets"),
+            label: "Tickets",
+          ),
           BottomNavigationBarItem(
             icon: Icon(FluentSystemIcons.ic_fluent_person_accounts_regular),
-            activeIcon: Icon(FluentSystemIcons.ic_fluent_person_accounts_filled),
-            label: "Profile"), 
-        ] // a list
+            activeIcon: Icon(
+              FluentSystemIcons.ic_fluent_person_accounts_filled,
+            ),
+            label: "Profile",
+          ),
+        ], // a list
       ),
     );
   }
